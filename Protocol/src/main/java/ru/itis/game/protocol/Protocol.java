a4c2a63f853f1bf56582bab9dce4630cbdacf4ee
anatolik2509 <antonov250901@mail.ru>
2020-12-16 16:10:00 +0300

    protocol

--- Protocol/src/main/java/ru/itis/game/protocol/Protocol.java
+++ Protocol/src/main/java/ru/itis/game/protocol/Protocol.java
@@ -2,19 +2,23 @@ package ru.itis.game.protocol;
 
 public enum Protocol {
     START_OF_TURN(1),
-    END_OF_TURN(2);
-    PURSHASE(3);
-    SALE(4);
-    LAY_DOWN(5);
-    BUY_BACK(6);
-    GO_TO_JAIL(7);
-    CHANCE(8);
-    PUBCLIC_TREASURY(9);
+    END_OF_TURN(2),
+    PURCHASE(3),
+    SALE(4),
+    LAY_DOWN(5),
+    BUY_BACK(6),
+    GO_TO_JAIL(7),
+    CHANCE(8),
+    PUBLIC_TREASURY(9),
     BALANCE_CHANGE(10);
 
-    private int messagNum;
+    private int messageNum;
 
-    public int getMessagNum() {
-        return messagNum
+    public int getMessageNum() {
+        return messageNum;
+    }
+
+    Protocol(int messageNum) {
+        this.messageNum = messageNum;
     }
 }
